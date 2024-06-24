df6d4cfa4770a6ca819603ce7583cb16bc61c843
HabibFahim15 <habibfahim71@gmail.com>
2024-06-24 19:59:40 +0600

    all done scic insa allah

--- src/Components/Home/Services/Services.jsx
+++ src/Components/Home/Services/Services.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 const Services = () => {
   return (
@@ -6,10 +7,14 @@ const Services = () => {
 		<h1 className="text-3xl font-bold leading-none sm:text-4xl">Welcome to
 			<span className="text-green-600"> Met Office</span>
 		</h1>
-		<p className=" mt-8 mb-12 text-lg">This is our official website for our Employee and Human Resource Executive(Hr).Our all staff can work here and also see their work progress..And his Will get Salaray after every month.</p>
+		<p className=" mt-8 mb-12 text-lg">This is our official website for our Employee and Human Resource Executive(Hr).Our all staff can work here and also see their work progress..And his Will get Salaray after every month.
+		<br />
+		If you want to join with us first you have create a account. <br />
+		Then go to The DashBoard you will see your task
+		</p>
 		<div className="flex flex-wrap justify-center">
-			<button className="px-8 py-3 m-2 text-lg font-semibold rounded bg-green-600 text-gray-50">Get started</button>
-			<button className="px-8 py-3 m-2 text-lg border rounded text-gray-900 border-gray-300">Learn more</button>
+			<Link to={'register'} className="px-8 py-3 m-2 text-lg font-semibold rounded bg-green-600 text-gray-50">Register now</Link>
+			<Link to={'dashboard'} className="px-8 py-3 m-2 text-lg border rounded text-gray-900 border-gray-300"> Go Dashboard</Link>
 		</div>
 	</div>
 </section>
